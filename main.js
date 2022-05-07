d6cc59ef2d0a83ce937a0a56f75b254750188223
unknown <tarekchenafi@gmail.com>
2022-05-07 19:18:16 +0100

    1

--- main.js
+++ main.js
@@ -3,12 +3,27 @@ window.onload = function () {
   document.querySelector("h1").style.color = "blue";
 };
 
+console.error("erooooor");
+console.table(["tarek", "wassim"]);
+console.log("hello from JS file");
+
 /*
+classes:
+_______
+window
+document
+console
+*/
+
+/*
+page state:
+__________
 window.onload
 */
 
 /*
-output
+output:
+______
 window.alert() //danger not recomended
 document.write()
 consol.log()
