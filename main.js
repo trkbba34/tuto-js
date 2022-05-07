b6b9461bc274f10e234712fc2e6ab2ba84b583ad
unknown <tarekchenafi@gmail.com>
2022-05-07 17:00:01 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+  // execute en page load
+  document.querySelector("h1").style.color = "blue";
+};
+
+/*
+window.onload
+*/
+
+/*
+output
+window.alert() //danger not recomended
+document.write()
+consol.log()
+*/
